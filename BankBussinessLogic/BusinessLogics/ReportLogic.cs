20d660f1acf6c084d31341008f15318ee04fca7e
sheronova-anastasia <anastasiaaa4278@gmail.com>
2020-10-10 01:03:53 +0400

    s

--- BankBussinessLogic/BusinessLogics/ReportLogic.cs
+++ BankBussinessLogic/BusinessLogics/ReportLogic.cs
@@ -135,13 +135,13 @@ namespace ZooBussinessLogic.BusinessLogics
         }
         public void SendMail(string email, string fileName, string subject)
         {
-            MailAddress from = new MailAddress("laba7aaa@gmail.com", "Зоопарк «Юрский период»");
+            MailAddress from = new MailAddress("labwork15kafis@gmail.com", "Зоопарк «Юрский период»");
             MailAddress to = new MailAddress(email);
             MailMessage m = new MailMessage(from, to);
             m.Subject = subject;
             m.Attachments.Add(new Attachment(fileName));
             SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            smtp.Credentials = new NetworkCredential("laba7aaa@gmail.com", "QWERTy12345!");
+            smtp.Credentials = new NetworkCredential("labwork15kafis@gmail.com", "passlab15");
             smtp.EnableSsl = true;
             smtp.Send(m);
         }
